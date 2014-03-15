3fe89b8f6b421c9aba7b47a720fb8281ecad474c
Anoop Elias <anoopelias@gmail.com>
2014-03-15 19:24:37 +0530

    Missed a file to checkin

---
+++ src/main/java/jredis/ZsetHelper.java
@@ -0,0 +1,21 @@
+package jredis;
+
+import jredis.exception.InvalidCommand;
+
+/**
+ * Helper class for zset based commands.
+ * 
+ * @author anoopelias
+ *
+ */
+public class ZsetHelper {
+
+    public static ElementSet get(String key) throws InvalidCommand {
+        try {
+            return DataMap.INSTANCE.get(key, ElementSet.class);
+        } catch (ClassCastException e) {
+            throw new InvalidCommand("Key already set as another type");
+        }
+    }
+
+}
