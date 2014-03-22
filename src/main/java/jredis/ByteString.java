12c5780f4864a3b6fbd16af47166795342726cd1
Anoop Elias <anoopelias@gmail.com>
2014-03-22 09:07:46 +0530

    Added get bit test

--- src/main/java/jredis/ByteString.java
+++ src/main/java/jredis/ByteString.java
@@ -4,12 +4,23 @@ package jredis;
  * ByteString data structure to handle both string and bit string.
  * 
  * @author anoopelias
- *
+ * 
  */
 public class ByteString {
-    
+
+    private static final int BYTE_SIZE = 8;
+    private static final int INIT_SIZE = 256;
     private byte[] value;
-    
+
+    /**
+     * Bit constructor.
+     * 
+     * @param value
+     */
+    public ByteString() {
+        this.value = new byte[INIT_SIZE];
+    }
+
     /**
      * String constructor.
      * 
@@ -28,13 +39,47 @@ public class ByteString {
         this.value = value;
     }
 
-    /* (non-Javadoc)
+    /**
+     * Get bit value at offset.
+     * 
+     * @param offset
+     * @return bit value (0 or 1). 0 if offset is bigger than current size.
+     */
+    public int getBit(int offset) {
+        if (value == null)
+            return 0;
+
+        int bytePos = offset / BYTE_SIZE;
+        int bitPos = offset % BYTE_SIZE;
+
+        if (value.length < bytePos)
+            return 0;
+
+        return isBitSet(value[bytePos], bitPos);
+    }
+
+    /**
+     * Ref :
+     * http://stackoverflow.com/questions/1034473/java-iterate-bits-in-byte
+     * -array
+     * 
+     * @param b
+     * @param bit
+     * @return
+     */
+    private static int isBitSet(byte b, int bit) {
+        return (b & (1 << BYTE_SIZE - (bit + 1))) != 0 ? 1 : 0;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
      * @see java.lang.Object#toString()
      */
     public String toString() {
-        if(value == null)
+        if (value == null)
             return null;
-        
+
         return Protocol.toString(value);
     }
 
--- src/test/java/jredis/ByteStringTest.java
+++ src/test/java/jredis/ByteStringTest.java
@@ -7,7 +7,10 @@ import java.io.UnsupportedEncodingException;
 import org.junit.Test;
 
 public class ByteStringTest {
-    
+
+    private static final byte[] BYTE = { 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59 };
+    private static final String BIT = "01010000010100010101001001010011010101000101010101010110010101110101100001011001";
+
     @Test
     public void test_string() {
         ByteString byteString = new ByteString("Mathew Perry");
@@ -16,8 +19,31 @@ public class ByteStringTest {
 
     @Test
     public void test_bytes() throws UnsupportedEncodingException {
-        ByteString byteString = new ByteString("Mathew Perry".getBytes(Protocol.CHARSET));
+        ByteString byteString = new ByteString(
+                "Mathew Perry".getBytes(Protocol.CHARSET));
         assertEquals("Mathew Perry", byteString.toString());
+    }
+
+    @Test
+    public void test_getbit_null() throws UnsupportedEncodingException {
+        ByteString byteString = new ByteString();
+
+        assertEquals(0, byteString.getBit(0));
+        assertEquals(0, byteString.getBit(10));
+        assertEquals(0, byteString.getBit(100));
+        assertEquals(0, byteString.getBit(1000));
+        assertEquals(0, byteString.getBit(10000));
+    }
+
+    @Test
+    public void test_getbit() throws UnsupportedEncodingException {
+        ByteString byteString = new ByteString(BYTE);
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<80; i++) 
+            sb.append(byteString.getBit(i));
+        
+        assertEquals(BIT, sb.toString());
     }
 
 }
