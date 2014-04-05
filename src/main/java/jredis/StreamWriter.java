4c97f8d685c675080c71a62fdf9b99b926fe038a
Anoop Elias <anoopelias@gmail.com>
2014-04-06 02:25:30 +0530

    Added <256 test case

--- src/main/java/jredis/StreamWriter.java
+++ src/main/java/jredis/StreamWriter.java
@@ -4,19 +4,45 @@ import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
+/**
+ * Write data into stream in RDF format.
+ * 
+ * @author anoopelias
+ * 
+ */
 public class StreamWriter {
-    
+
     private OutputStream os = null;
-    
+
+    /**
+     * Construct with a stream.
+     * 
+     * @param os
+     */
     public StreamWriter(OutputStream os) {
         this.os = new BufferedOutputStream(os);
     }
-    
+
+    /**
+     * Write a byte string.
+     * 
+     * @param b
+     * @throws IOException
+     */
     public void write(ByteString b) throws IOException {
         ByteArray by = b.toByteArray();
-        os.write((byte)(by.length()));
+        writeLen(by.length());
         by.write(os);
         os.flush();
+    }
+
+    private void writeLen(long num) throws IOException {
+        if (num >>> 6 == 0) // If the number is only 6 bits
+            os.write((byte) num);
+        else if (num >>> 14 == 0) {
+            os.write((int) (num >>> 8 | 0x40));
+            os.write((int) (num));
+        }
     }
 
 }
--- src/test/java/jredis/StreamWriterTest.java
+++ src/test/java/jredis/StreamWriterTest.java
@@ -10,6 +10,10 @@ import org.junit.Test;
 public class StreamWriterTest {
 
     private static final String LOREM_IPSUM_UNDER_63 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+    private static final String LOREM_IPSUM_UNDER_256 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+            + "Integer rutrum odio quis tortor sodales, in rutrum tortor gravida. "
+            + "Proin feugiat porttitor nunc nec consequat. "
+            + "Nulla aliquam consequat lorem, vitae luctus dolor gravida ut.";
 
     @Test
     public void test_write_byte_string_1_char() throws IOException {
@@ -33,10 +37,27 @@ public class StreamWriterTest {
 
         assertEquals(57, by.length);
         assertEquals(0x38, by[0]);
-        
+
         byte[] ex = TestUtil.toBytes(LOREM_IPSUM_UNDER_63);
         for (int i = 0; i < ex.length; i++)
             assertEquals(ex[i], by[i + 1]);
+    }
+
+    @Test
+    public void test_write_byte_string_under_256_char() throws IOException {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        StreamWriter sw = new StreamWriter(baos);
+        sw.write(new ByteString(LOREM_IPSUM_UNDER_256));
+        byte[] by = baos.toByteArray();
+
+        assertEquals(231, by.length);
+        assertEquals(0x40, by[0]);
+        assertEquals((byte) 0xe5, by[1]);
+
+        byte[] ex = TestUtil.toBytes(LOREM_IPSUM_UNDER_256);
+        for (int i = 0; i < ex.length; i++)
+            assertEquals(ex[i], by[i + 2]);
     }
 
 }
